ca6c2dca8d28148cdc02beeaedc5669328c6ed4a
Dane Pieri <dpieri@gmail.com>
2015-10-14 12:36:49 -0700

    Include simple autocomplete field

--- app/assets/javascripts/components/tag_photo.js.jsx
+++ app/assets/javascripts/components/tag_photo.js.jsx
@@ -3,9 +3,27 @@ var TagPhoto = React.createClass({
     url: React.PropTypes.string
   },
 
+  componentDidMount: function() {
+    $('#testMe').typeahead({
+      source: commonWords,
+      items: 5,
+      updater: function(item) {
+        console.log('updated');
+        // I think this is where we submit the item
+        return item;
+      },
+      afterSelect: function(item) {
+        console.log('after select');
+        return item;
+      }
+    })
+  },
+
   render: function() {
+    var testSource = ["Apple", "Bannana", "Pizza"];
     return (
       <div>
+        <input type="text" data-provide="typeahead" id="testMe" autoComplete="off" /> <br/>
         <img src={this.props.url} />
       </div>
     );
