d461320da1ccba3b37db98ab80c969c2c058ed58
Dane Pieri <dpieri@gmail.com>
2015-10-15 10:45:29 -0700

    Fake Flickr responses so I can work on this on the plane

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
   def tag
     photo = fetch_random_photo # This is in ApplicationController
     url_from_flickr_photo(photo)
-    @flickr_id = photo.id
+    @flickr_id = photo["id"]
     get_tagged_photos
   end
 
