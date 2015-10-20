a29ca117badde3f9b5c6a6ab32672b70fde55e84
Dane Pieri <dpieri@gmail.com>
2015-10-19 19:09:25 -0700

    Use new 350k word list

--- app/models/search_suggestion.rb
+++ app/models/search_suggestion.rb
@@ -2,19 +2,31 @@ class SearchSuggestion
 
   def self.seed
     $redis.flushall
-    File.open(Rails.root.join('lib', 'common_words.txt')).each_line do |line|
-      text = line.chop
-      1.upto(text.length) do |n|
-        prefix = text[0, n]
-        # Score is 2 for an exact match, 1 for everything else
-        score = n == text.length ? 2 : 1
-        $redis.zadd "search-suggestions:#{prefix.downcase}", score, text.downcase
+    File.open(Rails.root.join('lib', '350k_words.txt')).each_line do |line|
+      text = line.chop.downcase
+      length = text.length
+      [1, length - 5].max.upto(length) do |n|
+        prefix = text[0, n].downcase
+        # Scores
+        # Exact match = 5
+        # 1 letter short = 4
+        # 2 letters short = 3
+        # etc...
+        score = nil
+        if prefix == text
+          score = 5
+        else
+          diff = length - n
+          score = [1, 5-diff].max
+        end
+        # puts "adding search-suggestions:#{prefix} : #{text} #{score}"
+        $redis.zadd "ss:#{prefix}", score, text
       end
     end
   end
 
   def self.terms_for(prefix)
-    $redis.zrevrange "search-suggestions:#{prefix.downcase}", 0, 5
+    $redis.zrevrange "ss:#{prefix.downcase}", 0, 5
   end
 
 end
